92fee4d2a614784903df0d797f83f6e8ee7e4931
zhanggen <zhanggen@didichuxing.com>
2018-12-09 22:24:10 +0800

    del

--- main.go
+++ main.go
@@ -10,18 +10,8 @@ func sayHelloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "helloworld")
 }
 
-func sayHelloZhanggen(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello zhanggen")
-}
-
-func sayHelloJenkins(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello jenkins")
-}
-
 func main() {
 	http.HandleFunc("/", sayHelloWorld)
-	http.HandleFunc("/zhanggen", sayHelloZhanggen)
-	http.HandleFunc("/jenkins", sayHelloJenkins)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
