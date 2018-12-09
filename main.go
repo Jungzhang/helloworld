9e6008ff056f0a2e5aa59a33e57594558060f09a
zhanggen <zhanggen@didichuxing.com>
2018-12-09 20:53:35 +0800

    add hello jenkins

--- main.go
+++ main.go
@@ -14,9 +14,14 @@ func sayHelloZhanggen(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello zhanggen")
 }
 
+func sayHelloJenkins(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello zhanggen")
+}
+
 func main() {
 	http.HandleFunc("/", sayHelloWorld)
 	http.HandleFunc("/zhanggen", sayHelloZhanggen)
+	http.HandleFunc("/jenkins", sayHelloJenkins)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
