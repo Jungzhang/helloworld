8a01cdfba213aeeff57b0d6b45037edeabc2be55
zhanggen <zhanggen@didichuxing.com>
2018-12-09 15:59:04 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+app="helloworld"
+out="output"
+
+function build() {
+    go build -o ${app} main.go
+    local ret=$?
+    if [ ${ret} -ne 0 ];then
+        echo "$app build error"
+        exit ${ret}
+    else
+        echo -n "$app build ok"
+    fi
+    mkdir -p ${out}
+    mv ${app} ${out}
+}
+
+build
