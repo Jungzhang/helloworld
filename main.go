ccef4c1c7090d295fcc0f06b558d3962a0f145a3
zhanggen <zhanggen@didichuxing.com>
2018-12-09 16:07:36 +0800

    hello zhanggen

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ func sayHelloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "helloworld")
 }
 
+func sayHelloZhanggen(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello zhanggen")
+}
+
 func main() {
 	http.HandleFunc("/", sayHelloWorld)
+	http.HandleFunc("/zhanggen", sayHelloZhanggen)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
